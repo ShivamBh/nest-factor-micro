dae65371942a7e796fbcfa679c947d22c1e8526a
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-10-12 15:40:10 +0530

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  SwaggerModule.setup('api/v1/', app, createDocument(app));
+  await app.listen(process.env.PORT || 3000);
+  console.info('SERVER IS RUNNING ON PORT', process.env.PORT || 3000);
 }
 bootstrap();
