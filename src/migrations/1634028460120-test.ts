6247c2beabda5bc72dc92364c9a2fc9b309aa9f4
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-10-12 15:40:33 +0530

    init db, domain mod(trivial)

---
+++ src/migrations/1634028460120-test.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class test1634028460120 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
